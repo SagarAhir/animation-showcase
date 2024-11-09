07f04ee838cfb3f7e1356007c59895713af77003
SagarAhir <sagar1ahir@gmail.com>
2024-11-09 13:10:11 +0530

    Initial commit

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,7 +1,6 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Text, View } from '@/components/Themed';
 
 export default function NotFoundScreen() {
   return (
@@ -11,7 +10,6 @@ export default function NotFoundScreen() {
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
         <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
     </>
